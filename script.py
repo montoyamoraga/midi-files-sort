5e1053059d7b3633135d0b9a17d37ed463de2671
aaron montoya-moraga <montoyamoraga@gmail.com>
2021-12-13 16:51:18 -0300

    add header

--- script.py
+++ script.py
@@ -1,7 +1,13 @@
+# Python script for TODO
+# December 2021
+# runs on Python 3.x on Mac OS 12.0.1
+
 # import MidiFile from mido module
 from mido import MidiFile
 
+# import M
 from mido import MetaMessage
+
 
 # open the file
 myFile = MidiFile("A41emP.mid")
