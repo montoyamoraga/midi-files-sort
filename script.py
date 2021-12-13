50b098538abf8e851d005b71a09a25e247910216
aaron montoya-moraga <montoyamoraga@gmail.com>
2021-12-13 16:48:31 -0300

    add draft

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
+# import MidiFile from mido module
+from mido import MidiFile
+
+from mido import MetaMessage
+
+# open the file
+myFile = MidiFile("A41emP.mid")
+
+for i, track in enumerate(myFile.tracks):
+    print('Track {}: {}'.format(i, track.name))
+    for msg in track:
+        if msg.is_meta:
+          print(msg)
+
 print("hello world")
